778092e3868750b2f1d5b2d612b17ea19ade798e
CSRuggeri <santiruu@gmail.com>
2023-11-15 17:47:09 -0300

    formularios

--- app.js
+++ app.js
@@ -8,6 +8,12 @@ app.use("/static", express.static(__dirname + "/src/public"))
 app.get('/', (req,res)=>{
     res.sendFile(__dirname + "/src/views/home.html");
 });
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + "/src/views/login.html");
+});
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + "/src/views/register.html");
+});
 
 
 app.listen(3000, ()=>{
