dc19ccadc804b43955cf64425cb80c87c8c82081
CSRuggeri <santiruu@gmail.com>
2023-12-05 18:31:44 -0300

    falta el carro de compras nomas

--- src/routes/main.js
+++ src/routes/main.js
@@ -5,10 +5,10 @@ const controller= require('../controllers/mainController');
 router.get('/', controller.home);
 router.get('/login',controller.login);
 router.get('/register',controller.register);
-router.get('/edit-product',controller.editProduct);
+router.get('/create',controller.editProduct);
 router.get('/shopping-cart',controller.shoppingCart);
 router.get('/pagos',controller.pagos);
-router.get('/detail',controller.detail);
-
+router.get('/detail/:id',controller.detail);
+router.get('/add-to-cart/:id', controller.addToCart);
 
 module.exports= router
