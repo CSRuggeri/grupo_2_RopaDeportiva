339ac2d137be33693afeb35311921e472ed0b160
AFCC28 <142193041+af-coronel@users.noreply.github.com>
2024-01-23 22:48:00 -0300

    Se modificó usersController.js

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -2,63 +2,71 @@
 const fs = require('fs');
 const path = require('path');
 const localStorage = require('localStorage');
+const bcrypt = require('bcrypt');
+const { User } = require('../models');
 
 const usersFilePath = path.join(__dirname, '../data/users.json');
 
 const usersController = {
+  register: async (username, password, email, birthDate, address, profile, avatar) => {
+    try {
+      const hashedPassword = await bcrypt.hash(password, 10);
 
+      // Mantenemos la lógica para leer y escribir en el archivo dentro del bloque try
+      const usersData = fs.readFileSync(usersFilePath, 'utf-8');
+      const data = JSON.parse(usersData);
 
-  register: (username, password, email, birthDate, address, profile, avatar) => {
-    const usersData = fs.readFileSync(usersFilePath, 'utf-8');
-    const data = JSON.parse(usersData);
-  
+      if (!Array.isArray(data.users)) {
+        console.error('Users is not an array:', data.users);
+        throw new Error('Internal Server Error');
+      }
 
-    if (!Array.isArray(data.users)) {
-      console.error('Users is not an array:', data.users);
+      const id = data.users.length + 1;
+
+      const newUser = {
+        id,
+        username,
+        password: hashedPassword,
+        email,
+        birthDate,
+        address,
+        profile,
+        avatar,
+      };
+    
+      data.users.push(newUser);
+    
+      fs.writeFileSync(usersFilePath, JSON.stringify(data, null, 2), 'utf-8');
+    
+      return newUser;
+    } catch (error) {
+      console.error('Error al registrar usuario:', error);
       throw new Error('Internal Server Error');
     }
-  
-    
-    const id = data.users.length + 1;
-  
-    
-    const newUser = {
-      id,
-      username,
-      password, 
-      email,
-      birthDate,
-      address,
-      profile,
-      avatar, 
-    };
-  
-    
-    data.users.push(newUser);
-  
-    
-    fs.writeFileSync(usersFilePath, JSON.stringify(data, null, 2), 'utf-8');
-  
-    return newUser;
   },
 
- 
-  authenticate: (username, password) => {
+  authenticate: async (username, password) => {
     console.log('Attempting to authenticate:', username, password);
 
-    const usersData = fs.readFileSync(usersFilePath, 'utf-8');
-    const users = JSON.parse(usersData).users;
-    const user = users.find((u) => u.username === username && u.password === password);
+    try {
+      const usersData = fs.readFileSync(usersFilePath, 'utf-8');
+      const users = JSON.parse(usersData).users;
+      const hashedPassword = await bcrypt.hash(password, 10);
 
-    console.log('Found user:', user);
+      const user = users.find((u) => u.username === username && u.password === hashedPassword);
 
-    return user;
-},
+      console.log('Found user:', user);
+
+      return user;
+    } catch (error) {
+      console.error('Error al autenticar usuario:', error);
+      throw new Error('Internal Server Error');
+    }
+  },
+
   handleRegistration: (req, res) => {
     try {
       const { username, password, email, birthDate, address, profile } = req.body;
-console.log(req.body)
-     
       const { filename } = req.file;
 
       const newUser = usersController.register(
@@ -68,53 +76,48 @@ console.log(req.body)
         birthDate,
         address,
         profile,
-        `/images/show/${filename}` 
+        `/images/show/${filename}`
       );
 
-     
       res.redirect('/login');
     } catch (error) {
-      
       console.error(error.message);
       res.status(400).send(error.message);
     }
   },
+
   getUserProfile: (req, res) => {
     const user = localStorage.getItem('USER_INFO');
     const userInfo = user ? JSON.parse(user) : null;
 
-    // Check if user is not logged in
     if (!userInfo) {
-       alert("Debes iniciar sesion primero!")
-        res.redirect('/login');
-        return;
+      alert("Debes iniciar sesión primero!");
+      res.redirect('/login');
+      return;
     }
 
-    // User is logged in, render the dashboard
     res.render("dashboard.ejs", { user: userInfo });
-},
-  
-handleLogin: (req, res) => {
+  },
+
+  handleLogin: (req, res) => {
     try {
-        const { username, password } = req.body;
-        console.log('Received form data:', { username, password });
+      const { username, password } = req.body;
+      console.log('Received form data:', { username, password });
 
-        const authenticatedUser = usersController.authenticate(username, password);
+      const authenticatedUser = usersController.authenticate(username, password);
 
-        if (authenticatedUser) {
-            // Save user information in localStorage
-            localStorage.setItem('USER_INFO', JSON.stringify(authenticatedUser));
-            console.log(localStorage.getItem("USER_INFO"));
-
-            res.redirect('/dashboard');
-        } else {
-            res.status(401).send('Invalid credentials');
-        }
+      if (authenticatedUser) {
+        localStorage.setItem('USER_INFO', JSON.stringify(authenticatedUser));
+        console.log(localStorage.getItem("USER_INFO"));
+        res.redirect('/dashboard');
+      } else {
+        res.status(401).send('Invalid credentials');
+      }
     } catch (error) {
-        console.error(error.message);
-        res.status(500).send('Internal Server Error');
+      console.error(error.message);
+      res.status(500).send('Internal Server Error');
     }
-}
+  },
 };
 
 module.exports = usersController;
