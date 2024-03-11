0df13c5cb625d18eb2df557d413bafe8346b3b2c
af-coronel <alvaro.coronel@mi.unc.edu.ar>
2024-03-11 18:51:32 -0300

    Fix const

--- src/routes/productRouter.js
+++ src/routes/productRouter.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const productController = require('../controllers/productController');
 const {uploadProduct} = require('../Middlewares/Middlewares');
-const { body, validationResult } = require('express-validator');
+const { body } = require('express-validator');
 
 // Validaciones
 const commonValidations = [
