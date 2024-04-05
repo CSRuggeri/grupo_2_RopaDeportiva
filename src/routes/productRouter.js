7f0b5137120083a219db710774f1cdb857fabc5f
af-coronel <alvaro.coronel@mi.unc.edu.ar>
2024-04-05 09:58:12 -0300

    fix productRouter.js

--- src/routes/productRouter.js
+++ src/routes/productRouter.js
@@ -6,24 +6,47 @@ const { body } = require('express-validator');
 const isAdmin = require ('../Middlewares/GetAdmin');
 const isLoggedIn = require('../Middlewares/isLoggedIn');
 
-// Validaciones
-const commonValidations = [
-    body('name').trim().isLength({ min: 3 }).withMessage('El nombre debe tener al menos 3 caracteres'),
-    body('price').isNumeric().withMessage('El precio debe ser un número positivo'),
-    body('stock').isInt({ min: 1 }).withMessage('El stock debe ser un número entero positivo'),
-    body('description').trim().isLength({ min: 10 }).withMessage('La descripción debe tener al menos 10 caracteres'),
-    body('category_id').isInt({ min: 1 }).withMessage('Seleccione una categoría válida'),
-    body('brand').isInt({ min: 1 }).withMessage('Seleccione una marca válida')
+// Validaciones para la creación del producto
+
+  const productValidations = [
+    body('name')
+      .notEmpty().withMessage('El nombre del producto es obligatorio')
+      .isLength({ max: 45 }).withMessage('El nombre del producto no puede superar los 45 caracteres'),
+    body('price')
+      .notEmpty().withMessage('El precio del producto es obligatorio')
+      .isInt({ min: 0 }).withMessage('El precio del producto debe ser un número entero mayor o igual a 0'),
+    body('stock')
+      .notEmpty().withMessage('El stock del producto es obligatorio')
+      .isInt({ min: 1 }).withMessage('El stock del producto debe ser un número entero mayor o igual a 1'),
+    body('description')
+      .notEmpty().withMessage('La descripción del producto es obligatoria')
+      .isLength({ max: 150 }).withMessage('La descripción del producto no puede superar los 150 caracteres'),
+    body('gender')
+      .notEmpty().withMessage('El género del producto es obligatorio')
+      .isLength({ max: 45 }).withMessage('El género del producto no puede superar los 45 caracteres'),
+    body('image')
+      .notEmpty().withMessage('La imagen del producto es obligatoria')
+      .isLength({ max: 150 }).withMessage('La URL de la imagen no puede superar los 150 caracteres'),
+    body('discount')
+      .optional()
+      .isLength({ max: 45 }).withMessage('El descuento no puede superar los 45 caracteres'),
+    body('size')
+      .notEmpty().withMessage('La talla del producto es obligatoria')
+      .isLength({ max: 45 }).withMessage('La talla del producto no puede superar los 45 caracteres'),
+    //body('category_id')
+    // .isInt({ min: 1 }).withMessage('La categoría del producto debe ser un número entero mayor a 0')
+    //body('brand_id')
+    // .isInt({ min: 1 }).withMessage('La marca del producto debe ser un número entero mayor a 0'),
   ];
 
 router.get('/', productController.index);
 
 router.get('/create',isLoggedIn, productController.createProduct);
+
 /*api*/
 router.get("/api", productController.getAllProductsAPI)
 router.post('/api',uploadProduct.single('image'), 
-commonValidations, productController.createProductAPI)  
-
+productValidations, productController.createProductAPI)  
 
 
 router.get("/:id", productController.detail);
@@ -31,7 +54,7 @@ router.get("/:id", productController.detail);
 router.post(
     '/create', 
     uploadProduct.single('image'), 
-    commonValidations,
+    productValidations,
     productController.store
   );
 
@@ -40,15 +63,11 @@ router.get('/:id/edit', productController.edit)
 
 router.put(
     '/:id/update',
-    uploadProduct.single('image'),
-    commonValidations,
+    uploadProduct.single('image'),productValidations,
     productController.update
   );
 
 router.delete('/:id/delete', productController.destroy);
-
-
-
 
 
 module.exports = router
